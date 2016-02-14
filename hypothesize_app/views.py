e25fb05a66d871d03253f23366d8f3e8aaffd827
Rich Pang <rpang.contact@gmail.com>
2016-02-13 22:09:26 -0800

    Add index view and empty document search and node search views.

--- hypothesize_app/views.py
+++ hypothesize_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'hypothesize_app/index.html')
+
+def document_search(request):
+    pass
+
+def node_search(request):
+    pass
