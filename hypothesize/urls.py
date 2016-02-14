100262994d4980122a76bd73095dceaa88b54cc0
Rich Pang <rpang.contact@gmail.com>
2016-02-14 11:38:41 -0800

    Remove deprecated usage of patterns.

--- hypothesize/urls.py
+++ hypothesize/urls.py
@@ -13,16 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
 import settings
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^hypothesize/', include('hypothesize_app.urls', namespace='hypothesize_app')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- hypothesize_app/urls.py
+++ hypothesize_app/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from hypothesize_app import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^document/search/$', views.document_search, name='document_search'),
     url(r'^node/search/$', views.node_search, name='node_search'),
-)
+]
