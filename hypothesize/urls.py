cf9f3359e424992b45290fe549b1c76cde786dba
Rich Pang <rpang.contact@gmail.com>
2016-02-13 22:08:34 -0800

    Add basic urls.

--- hypothesize/urls.py
+++ hypothesize/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+import settings
+
+urlpatterns = patterns('',
+    url(r'^hypothesize/', include('hypothesize_app.urls', namespace='hypothesize_app')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
