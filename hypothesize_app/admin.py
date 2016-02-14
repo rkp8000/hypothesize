386ed38fec451b259e771f5f80586483d2ed0a28
Rich Pang <rpang.contact@gmail.com>
2016-02-14 11:50:57 -0800

    Add NodeType and Node.

--- hypothesize_app/admin.py
+++ hypothesize_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Author, Document, Supplement
+from .models import Author, Document, Supplement, NodeType, Node
 
 admin.site.register(Author)
 admin.site.register(Document)
 admin.site.register(Supplement)
+admin.site.register(NodeType)
+admin.site.register(Node)
