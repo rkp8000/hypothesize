327352baf585e34358c11154895ba28342c7a62f
Rich Pang <rpang.contact@gmail.com>
2016-02-14 12:54:47 -0800

    Start document search view.

--- hypothesize_app/views.py
+++ hypothesize_app/views.py
@@ -1,11 +1,18 @@
+from __future__ import division, print_function
 from django.shortcuts import render
+import models
 
+DEFAULT_DOCUMENTS_TO_SHOW = models.Setting.objects.get(pk='DEFAULT_DOCUMENTS_TO_SHOW').value
 
 def index(request):
     return render(request, 'hypothesize_app/index.html')
 
-def document_search(request):
-    pass
+def document_search(request, n_articles=DEFAULT_DOCUMENTS_TO_SHOW):
+
+    context = {'documents': None,
+               'document_search_form': None,
+               }
+    return render(request, 'hypothesize_app/document_search.html', context)
 
 def node_search(request):
     pass
