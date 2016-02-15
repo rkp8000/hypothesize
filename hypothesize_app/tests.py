0c64a84f63d60664a396ce47ca584718bef1ff59
Rich Pang <rpang.contact@gmail.com>
2016-02-14 16:51:34 -0800

    Note some basic tests that I should add.

--- hypothesize_app/tests.py
+++ hypothesize_app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# document add
+## basic
+## with weird author names
+## with supplement
+## with document with same automatically generated primary key
+# document change
+## basic
+## with weird author names
+## with supplement
+## rearrange authors
+
+
+# node add
+# node change
