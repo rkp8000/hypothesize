a61d08e6215f2cec523b21dbf7780181eda1afdc
Rich Pang <rpang.contact@gmail.com>
2016-02-14 16:08:52 -0800

    Reinstate uber simple document search form and view.

--- hypothesize_app/views.py
+++ hypothesize_app/views.py
@@ -1,18 +1,45 @@
 from __future__ import division, print_function
 from django.shortcuts import render
+
+from forms import DocumentSearchForm
 import models
 
 DEFAULT_DOCUMENTS_TO_SHOW = models.Setting.objects.get(pk='DEFAULT_DOCUMENTS_TO_SHOW').value
 
+
 def index(request):
     return render(request, 'hypothesize_app/index.html')
 
-def document_search(request, n_articles=DEFAULT_DOCUMENTS_TO_SHOW):
 
-    context = {'documents': None,
-               'document_search_form': None,
+def document_search(request, n_documents=DEFAULT_DOCUMENTS_TO_SHOW):
+
+    # check if a search query has been sent
+    if request.GET:
+        # search some stuff
+        document_search_form = DocumentSearchForm(request.GET)
+        if document_search_form.is_valid():
+            documents = document_search_form.get_articles_from_query()
+        else:
+            documents = models.Document.objects.order_by('-last_viewed')[:n_documents]
+    else:
+        # make new search form
+        document_search_form = DocumentSearchForm()
+        # get most recently viewed articles
+        documents = models.Document.objects.order_by('-last_viewed')[:n_documents]
+
+    context = {'documents': documents,
+               'document_search_form': document_search_form,
                }
     return render(request, 'hypothesize_app/document_search.html', context)
+
+
+def document_view(request, document_id):
+    pass
+
+
+def document_add(request):
+    pass
+
 
 def node_search(request):
     pass
