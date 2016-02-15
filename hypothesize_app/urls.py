1578906e8e08fef94bc9c900cf828125576e34fd
Rich Pang <rpang.contact@gmail.com>
2016-02-14 16:09:55 -0800

    Add "add" and "view" urls for documents.

--- hypothesize_app/urls.py
+++ hypothesize_app/urls.py
@@ -5,5 +5,7 @@ from hypothesize_app import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^document/search/$', views.document_search, name='document_search'),
+    url(r'^document/add/$', views.document_add, name='document_add'),
+    url(r'^document/(?P<document_id>[\w-]+)/view/$', views.document_view, name='document_view'),
     url(r'^node/search/$', views.node_search, name='node_search'),
 ]
