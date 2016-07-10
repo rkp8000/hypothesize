5be42c2755d2ffbf3349c7323e7738a99edc5f04
rkp8000 <rpang.contact@gmail.com>
2016-07-09 18:31:31 -0700

    Add module for user-defined settings.

---
+++ hypothesize/my_settings.py
@@ -0,0 +1,16 @@
+"""
+Set all of your personal settings here.
+"""
+
+# username and password (note: these are not secure since they are stored as plain text)
+
+USERNAME = ''
+PASSWORD = ''
+
+# where to save files corresponding to nodes
+
+NODE_DIRECTORY = ''
+
+# where to store database backups
+
+DATABASE_BACKUP_DIRECTORY = ''
