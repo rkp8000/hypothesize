4db1c42ed468584d47aa0af4a06446f5c91f3425
rkp8000 <rpang.contact@gmail.com>
2016-07-14 16:33:02 -0700

    Add nonfunctional abstract search function.

--- hypothesize_app/crossref_search.py
+++ hypothesize_app/crossref_search.py
@@ -2,10 +2,24 @@
 Functions for searching crossref for document metadata.
 """
 from difflib import SequenceMatcher
+import json
 
 from habanero import Crossref
 
 N_TOP_ITEMS = 5
+
+
+def get_abstract(item):
+    """
+    NOT IMPLEMENTED YET
+
+    Attempt to find the item's abstract using a combination of biopython and web scraping.
+
+    :param item: crossref item corresponding to a document
+    :return: abstract string if found, otherwise empty string
+    """
+
+    return ''
 
 
 def get_metadata_from_title(title):
@@ -24,4 +38,8 @@ def get_metadata_from_title(title):
 
     top_item = sorted(top_items, key=match)[-1]
 
-    return top_items, top_item
+    # attempt to find abstract
+
+    top_item['abstract'] = get_abstract(top_item)
+
+    return top_item
